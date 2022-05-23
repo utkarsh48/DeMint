732f54e658c83e48b6f66860b2417c856b9b9525
Utkarsh Dhiman <utkarshdhiman48@gmail.com>
2022-05-23 19:57:03 +0530

    add: icon for docs

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
-import { CardList, Upload } from 'react-bootstrap-icons';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { CardList, FileEarmarkText, Upload } from "react-bootstrap-icons";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   const links = [
     {
       icon: <CardList />,
-      to: '/',
-      name: 'NFTs'
+      to: "/",
+      name: "NFTs",
     },
     {
       icon: <Upload />,
-      to: '/mint',
-      name: 'Mint'
+      to: "/mint",
+      name: "Mint",
+    },
+    {
+      icon: <FileEarmarkText />,
+      to: "/docs",
+      name: "Docs",
     },
   ];
   return (
     <aside
-      style={{ height: 'calc(100vh - 56px)', width: '5rem' }}
+      style={{ height: "calc(100vh - 56px)", width: "5rem" }}
       className="bg-white shadow-sm"
     >
-      {links.map(link=>(<NavLink key={link.to}
-        className="btn btn-outline-primary text-center border-0 rounded-0 p-2 py-3 w-100 shadow-none"
-        to={link.to}
-      >
-        {link.icon}
-        <div style={{fontSize: '0.8rem'}}>{link.name}</div>
-      </NavLink>))}
+      {links.map((link) => (
+        <NavLink
+          key={link.to}
+          className="btn btn-outline-primary text-center border-0 rounded-0 p-2 py-3 w-100 shadow-none"
+          to={link.to}
+        >
+          {link.icon}
+          <div style={{ fontSize: "0.8rem" }}>{link.name}</div>
+        </NavLink>
+      ))}
     </aside>
   );
 };
